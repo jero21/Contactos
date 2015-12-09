e97aa3a75fef4fbaf552941b6a3d1e7e598f306c
jero21 <jeremias.morac@gmail.com>
2015-12-09 17:13:54 -0300

    actualizacion

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=contacto_2',
+    'dsn' => 'mysql:host=localhost;dbname=snit',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
